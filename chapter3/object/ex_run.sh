5e4e170139c76d5b8f6b5ca49569b6c2c12fd417
n-guitar <nguitar001@gmail.com>
2022-06-15 12:50:28 +0900

    exam shell and yaml

--- chapter3/object/ex_run.sh
+++ chapter3/object/ex_run.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 EX_ID=$1
+BRANCH=$2
 RET=0
 
 if [ -z $EX_ID ]; then
@@ -9,9 +10,16 @@ if [ -z $EX_ID ]; then
   exit ${RET}
 fi
 
+if [ -z $BRANCH ]; then
+  echo "please set branch!"
+  RET=1
+  exit ${RET}
+fi
+
 
 if [ $EX_ID == "ex2" ]; then
   echo "set ex2 objects!"
+  kubectl apply -f https://raw.githubusercontent.com/n-guitar/k8s-bootcamp/$BRANCH/chapter3/object/ex2-pod.yaml
   exit ${RET}
 fi
 
